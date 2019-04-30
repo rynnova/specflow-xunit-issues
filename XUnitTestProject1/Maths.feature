0c5ed008f7a91cb4ab9b241d50d01049043b66d0
Ryan Y <ryayak1460@protingumas.org>
2019-04-30 07:59:57 -0400

    Create basic SpecFlow+xunit project
    Create a basic SpecFlow+xunit project to show the issues trying to
    combine these two tools.
    
    Signed-off-by: Ryan Y <ryayak1460@protingumas.org>

---
+++ XUnitTestProject1/Maths.feature
@@ -0,0 +1,6 @@
+﻿Feature: Maths
+    Scenario: Addition
+        Given x = 1
+        And y = 2
+        When x + y
+        Then 3
---
+++ XUnitTestProject1/MathsSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace XUnitTestProject1
+{
+    [Binding]
+    public class MathsSteps
+    {
+        [Given(@"x = (.*)")]
+        public void GivenX(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"y = (.*)")]
+        public void GivenY(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"x \+ y")]
+        public void WhenXY()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"(.*)")]
+        public void Then(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
